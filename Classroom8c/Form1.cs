6a0da94958fd5c0a991f6472575ab7845f6762e0
Robert Slamon <rslamon049@gmail.com>
2019-02-09 23:12:10 -0500

    Commit All

--- Classroom8c/Form1.cs
+++ Classroom8c/Form1.cs
@@ -12,9 +12,37 @@ namespace Classroom8c
 {
     public partial class Form1 : Form
     {
+        String[] myArray = {"Bob","Steve","Mike","Dave","Sally" };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int myInt = Convert.ToInt32(textBox1.Text);
+
+                //string strOne = myArray[myInt];
+
+                MessageBox.Show($"Array selected is " + myArray[myInt]);
+            }
+            catch(System.FormatException sfe)
+            {
+                MessageBox.Show("Dude!");
+            }
+
+            catch (System.IndexOutOfRangeException ior)
+            {
+                MessageBox.Show(ior.Message.ToString());
+            }
         }
     }
 }
